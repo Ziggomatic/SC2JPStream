00e0cf138e79d035c4ea2d774eaec8c3289a14e4
furuya <furuyah88@gmail.com>
2016-12-02 00:35:50 +0900

    Support ニコ生

--- app.py
+++ app.py
@@ -1,20 +1,12 @@
 # -*- coding: utf-8 -*-
 
-
-import json
-import urllib.request
 import tweepy
-import time
+import twitch
+import nico
 from datetime import datetime
-from pytz import timezone
 from os import environ
-
-twitch_api = 'https://api.twitch.tv/kraken/streams'
-onlySC2 = '?game=StarCraft+II'  # SC2を配信しているか
-isJapanese = '&language=ja'  # 日本語での配信
-limit = '&limit=5'  # 5人も同時に配信はしないでしょうという偏見
-client_id = environ['TWITCH_CLIENT_ID']  # twitchAPIのクライアントID
-
+from pytz import timezone
+from time import sleep
 
 consumer_key = environ['TWITTER_CONSUMER_KEY']
 consumer_secret = environ['TWITTER_CONSUMER_SECRET']
@@ -26,27 +18,26 @@ auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
 
+def post_twitch(now):
+    streams = twitch.streams(limit=5)
+    if len(streams) > 0:
+        lines = [now.strftime('%m/%d %X'), '[Twitch]']
+        lines.extend("{}:{}".format(x[0], x[1]) for x in streams)
+        api.update_status('\n'.join(lines))
 
+def post_nico(now):
+    streams = nico.streams(limit=5)
+    if len(streams) > 0:
+        lines = [now.strftime('%m/%d %X'), '[ニコ生]']
+        lines.extend("{}:{}".format(x[0], x[1]) for x in streams)
+        api.update_status('\n'.join(lines))
+
+# hourが変わるたびに投稿する
 last_hour = datetime.now(timezone('Asia/Tokyo')).hour
 while True:
-    jst_now = datetime.now(timezone('Asia/Tokyo'))
-    if jst_now.hour != last_hour: # hourが変わるたびにツイートする
-        now = jst_now.strftime('%m/%d %X')
-        res = urllib.request.urlopen(twitch_api + onlySC2 + limit + isJapanese + client_id)
-        j = json.loads(res.read().decode('utf8'))
-        if j['_total'] == 0:  # 該当配信が0の場合
-
-            api.update_status(str(now) + "\n" + "no one streaming. ")
-        else:  # 配信の数をカウント
-            total = j['_total']
-            list = []
-            for x in range(total):
-                name = j['streams'][x]['channel']['name']
-                url = j['streams'][x]['channel']['url']
-                line = name + ":" + url
-                list.append(line)
-                s = '\n'.join(list)
-            # 同じツイートをすると怒られるので申し訳程度にnow()を添える
-            api.update_status(str(now) + "\n" + s)
-    last_hour = jst_now.hour
-    time.sleep(600)
+    now = datetime.now(timezone('Asia/Tokyo'))
+    if now.hour != last_hour:
+        post_twitch(now)
+        post_nico(now)
+    last_hour = now.hour
+    sleep(600)
