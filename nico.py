83d7293862afa438cda39fdec5b205e3a2448c9f
furuya <furuyah88@gmail.com>
2016-12-01 22:22:11 +0900

    Fix nico.py

--- nico.py
+++ nico.py
@@ -4,21 +4,20 @@ import json
 from urllib.request import urlopen
 
 TAG = 'StarCraft2'
-LIMIT = 5
 
-# ニコ生検索
+# ニコ生配信を検索
 # (title, url)のタプル配列を返す
-def search():
+def streams(limit=5):
     url = "http://api.search.nicovideo.jp/api/v2/live/contents/search"
     url += "?q={}&targets=tags".format(TAG) # タグ
     url += "&filters[liveStatus][0]=onair" # 放送中のみ
     url += "&fields=contentId,title" # 欲しいデータ
     url += "&_sort=-startTime" # 開始時間でソート
-    url += "&_limit={}".format(LIMIT) # 件数
+    url += "&_limit={}".format(limit) # 件数
     url += "&_context=apiguide"
     res = urlopen(url)
     j = json.loads(res.read().decode('utf8'))
-    if j['meta']['status'] != 200:
+    if res.getcode() != 200:
         return []
     return [(x['title'], live_url(x['contentId'])) for x in j['data']]
 
