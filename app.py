6a5fedbbf92aee8719c22350ad9b2cd5dd6440f4
furuya <furuyah88@gmail.com>
2017-02-27 19:11:05 +0900

    Add post_safe

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from datetime import datetime
 from pytz import timezone
 from time import sleep
 
-def post(now, streams, header):
+def post_base(now, streams, header):
     streams = list(streams)
     if len(streams) <= 0:
         return
@@ -20,29 +20,32 @@ def post(now, streams, header):
     twitter.update_status('\n'.join(lines))
 
 def post_twitch(now):
-    post(now, twitch.streams(limit=3), '[Twitch]')
+    post_base(now, twitch.streams(limit=3), '[Twitch]')
 
 def post_nico(now):
-    post(now, nico.streams(limit=2), '[ニコ生]')
+    post_base(now, nico.streams(limit=2), '[ニコ生]')
 
 def post_openrec(now):
-    post(now, openrec.streams(limit=3), '[OPENREC]')
+    post_base(now, openrec.streams(limit=3), '[OPENREC]')
 
 def post_afreeca(now):
-    post(now, afreeca.streams(), '[AfreecaTV]')
+    post_base(now, afreeca.streams(), '[AfreecaTV]')
+
+def post_safe(postfunc, now):
+    try:
+        postfunc(now)
+    except Exception as e:
+        print("Unknown exception: {}".format(e), file=sys.stderr)
+        traceback.print_exc(file=sys.stderr)
 
 # hourが変わるたびに投稿する
 last_hour = datetime.now(timezone('Asia/Tokyo')).hour
 while True:
     now = datetime.now(timezone('Asia/Tokyo'))
     if now.hour != last_hour:
-        try:
-            post_twitch(now)
-            post_nico(now)
-            post_openrec(now)
-            post_afreeca(now)
-        except Exception as e:
-            print("Unknown exception: {}".format(e), file=sys.stderr)
-            traceback.print_exc(file=sys.stderr)
+        post_safe(post_twitch, now)
+        post_safe(post_nico, now)
+        post_safe(post_openrec, now)
+        post_safe(post_afreeca, now)
     last_hour = now.hour
     sleep(600)
